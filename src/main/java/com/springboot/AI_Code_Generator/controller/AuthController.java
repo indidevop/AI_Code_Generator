7412449d2a6971fc602ff0dd012cb52a43e3d44b
Amey Sawalkar <ameykishor@gmail.com>
2026-05-08 12:06:31 +0530

    Added controllers for auth and project (boiler plates)

---
+++ src/main/java/com/springboot/AI_Code_Generator/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.springboot.AI_Code_Generator.controller;
+
+import com.springboot.AI_Code_Generator.dto.auth.AuthResponse;
+import com.springboot.AI_Code_Generator.dto.auth.LoginRequest;
+import com.springboot.AI_Code_Generator.dto.auth.SignupRequest;
+import com.springboot.AI_Code_Generator.dto.auth.UserProfileResponse;
+import com.springboot.AI_Code_Generator.service.AuthService;
+import com.springboot.AI_Code_Generator.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> signup(SignupRequest request){
+        return ResponseEntity.ok(authService.signup(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(LoginRequest request){
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserProfileResponse> getProfile(){
+        Long userId=1L;
+        return ResponseEntity.ok(userService.getProfile(userId));
+    }
+}
---
+++ src/main/java/com/springboot/AI_Code_Generator/controller/ProjectController.java
@@ -0,0 +1,50 @@
+package com.springboot.AI_Code_Generator.controller;
+
+import com.springboot.AI_Code_Generator.dto.project.ProjectRequest;
+import com.springboot.AI_Code_Generator.dto.project.ProjectResponse;
+import com.springboot.AI_Code_Generator.dto.project.ProjectSummaryResponse;
+import com.springboot.AI_Code_Generator.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/project")
+public class ProjectController {
+    private final ProjectService projectService;
+
+    @GetMapping
+    public ResponseEntity<List<ProjectSummaryResponse>> getMyProjects(){
+        Long userId=1L;
+        return ResponseEntity.ok(projectService.getUserProjects(userId));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ProjectResponse> getProjectById(@PathVariable Long projectId){
+        Long userId=1L;
+        return ResponseEntity.ok(projectService.getUserProjectById(projectId, userId));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProjectResponse> createProject(@RequestBody ProjectRequest request){
+        Long userId=1L;
+        return ResponseEntity.status(HttpStatus.CREATED).body(projectService.createProject(userId,request));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ProjectResponse> updateProject(@PathVariable Long projectId, @RequestBody ProjectRequest request){
+        Long userId=1L;
+        return ResponseEntity.ok(projectService.updateProject(projectId, request, userId));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ProjectResponse> deleteProject(@PathVariable Long projectId){
+        Long userId=1L;
+        return ResponseEntity.ok(projectService.deleteProject(projectId,userId));
+    }
+}
+
