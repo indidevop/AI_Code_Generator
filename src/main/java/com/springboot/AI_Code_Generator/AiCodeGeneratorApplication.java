a0a85f2028b8e85a5e2e30cea3e2a70b08396dfa
Amey Sawalkar <ameykishor@gmail.com>
2026-05-08 11:24:15 +0530

    Let's begin

---
+++ src/main/java/com/springboot/AI_Code_Generator/AiCodeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.AI_Code_Generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCodeGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/AI_Code_Generator/AiCodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.AI_Code_Generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
