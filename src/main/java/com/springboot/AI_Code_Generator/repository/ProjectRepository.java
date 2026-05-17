5dbdff59b40d72329fb361aaa3c840f17807a7f2
Amey Sawalkar <ameykishor@gmail.com>
2026-05-17 10:17:33 +0530

    Added method and query to get projects for userid

--- src/main/java/com/springboot/AI_Code_Generator/repository/ProjectRepository.java
+++ src/main/java/com/springboot/AI_Code_Generator/repository/ProjectRepository.java
@@ -2,6 +2,19 @@ package com.springboot.AI_Code_Generator.repository;
 
 import com.springboot.AI_Code_Generator.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project,Long> {
+
+    @Query("""
+        select p from Project p
+        where p.owner.id=:userid AND
+        p.deletedAt IS NULL
+        ORDER BY p.updatedAt DESC
+        """)
+    List<Project> findAllAccessibleByUser(@Param("userid") Long userId);
+
 }
