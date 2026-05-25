14772df2064f2d357bee9dfb406e18f4ce5983b6
Amey Sawalkar <ameykishor@gmail.com>
2026-05-25 22:58:19 +0530

    added support for .env file

--- src/main/java/com/springboot/AI_Code_Generator/AiCodeGeneratorApplication.java
+++ src/main/java/com/springboot/AI_Code_Generator/AiCodeGeneratorApplication.java
@@ -2,8 +2,10 @@ package com.springboot.AI_Code_Generator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:.env")
 public class AiCodeGeneratorApplication {
 
 	public static void main(String[] args) {
